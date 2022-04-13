94ca713e9e1f5fa437d186f348b4b64837e00cd8
Rellyson Douglas <rellysondouglas2015@gmail.com>
2022-04-13 20:00:56 -0300

    Created Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,45 @@
+import { Container, Flex } from "@chakra-ui/react";
+import Head from "next/head";
+import { Image } from "./Image";
+
+type Props = {
+  pageName: string
+}
+
+export function Header({ pageName }: Props) {
+  return (
+    <Flex
+      as="header"
+      w="100%"
+      h="20"
+      bgColor="brand.black"
+      position="relative"
+    >
+      <Head>
+        <title>{pageName}</title>
+      </Head>
+
+      <Container
+        maxW="8xl"
+      >
+
+        <Image
+          src="/assets/logo-prefeitura.png"
+          alt="logo da prefeitura municipal de mossoró em conjunto com a Secretaria Municipal de Segurança Pública, Defesa Civil, Mobilidade Urbana e Trânsito (SESDEM)"
+          w={["40", "xs"]}
+          width="600"
+          height="258"
+
+          position="absolute"
+          bottom={["0", "-20"]}
+          left={["50%", "50%", "auto",]}
+
+          transform={["translate(-50%, 0%)", "translate(-50%, 0%)", "translate(0%, 0%)"]}
+
+          zIndex="10"
+        />
+
+      </Container>
+    </Flex>
+  )
+}
