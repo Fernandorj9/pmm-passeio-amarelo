550c133031ac53e4dde3474f581eee7ee12419e5
Rellyson Douglas <rellysondouglas2015@gmail.com>
2022-04-17 13:27:12 -0300

    added document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,7 @@ export default class Document extends NextDocument {
     return (
       <Html lang='pt-br'>
         <Head>
-          <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+          <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;700;800;900&family=Raleway:wght@100;300;500&family=Rubik:wght@500;600;700&display=swap" rel="stylesheet" />
